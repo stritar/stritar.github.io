9844dbd008ca20ef24f2f582022e24fbb38ec0d9
Denis <denis.stritar@gmail.com>
2014-07-29 16:09:18 +0200

    fgdg

--- STAGE/js/custom.js
+++
@@ -1,7 +0,0 @@
-var $window = $(window),
-       $stickyEl = $('#vignette'),
-       elTop = $stickyEl.offset().top;
-
-   $window.scroll(function() {
-        $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-    });
