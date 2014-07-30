aaa353f23c8a4200594b7ffc588f897728eccb37
Denis <denis.stritar@gmail.com>
2014-07-30 09:14:31 +0200

    gdfgdfg

---
+++ STAGE/js/custom.js
@@ -0,0 +1,6 @@
+$('.').toggle(function() { // this refer to the just clicked button.
+    $('button.active').click(); // Remove active class from all other buttons.
+    $(this).addClass("active-toggle");
+}, function() {
+    $(this).removeClass("active-toggle");
+});
