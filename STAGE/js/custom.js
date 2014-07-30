6791b406650375b67d718b400582e2a9399e8928
Denis <denis.stritar@gmail.com>
2014-07-30 14:38:46 +0200

    fgddfgd

--- STAGE/js/custom.js
+++ STAGE/js/custom.js
@@ -4,3 +4,8 @@ $('.').toggle(function() { // this refer to the just clicked button.
 }, function() {
     $(this).removeClass("active-toggle");
 });
+
+$('html,body').bind('mousewheel',function(ev, delta) {
+    var scrollTop = $(this).scrollTop();
+    $(this).scrollTop(scrollTop-Math.round(delta * 1));
+});
