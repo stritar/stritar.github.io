6db1ca9738d28dc6e2e33918dfb13766759a5896
stritar <Stritar@Deniss-MacBook-Pro.local>
2014-08-21 15:44:54 +0200

    Spring cleaning

--- STAGE/js/custom.js
+++
@@ -1,7 +0,0 @@
-$('.').toggle(function() { // this refer to the just clicked button.
-    $('button.active').click(); // Remove active class from all other buttons.
-    $(this).addClass("active-toggle");
-}, function() {
-    $(this).removeClass("active-toggle");
-});
-
