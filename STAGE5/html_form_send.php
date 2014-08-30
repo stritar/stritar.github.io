2d995ed43fa0fe8d6baa6575c25f842b961a5db3
Your Name <you@example.com>
2014-08-31 00:04:39 +0200

    sdfsf

--- STAGE5/html_form_send.php
+++ STAGE5/html_form_send.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
 	
 	// CHANGE THE TWO LINES BELOW
-	$email_to = "you@yourdomain.com";
+	$email_to = "denis@denisstritar.com";
 	
 	$email_subject = "website html form submissions";
 	
