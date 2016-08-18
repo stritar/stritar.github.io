7f8bf07cbef2ffc903444553ad22e076b9323378
stritar <Stritar@Deniss-MacBook-Pro.local>
2016-08-18 21:25:02 +0200

    dfs
    fdsfs

---
+++ STAGE/js/nav-state.js
@@ -0,0 +1,21 @@
+$(function () {
+    var sections = $("section");
+    var navigation_links = $("nav a");
+
+    sections.waypoint({
+        handler: function (direction) {
+
+            var active_section;
+            active_section = $(this);
+            if (direction === "up") active_section = active_section.prev();
+
+            var active_link = $('nav a[href="#' + active_section.attr("id") + '"]');
+            navigation_links.removeClass("selected");
+            active_link.addClass("selected");
+
+        },
+        offset: '50%'
+    })
+
+
+});
